ff0abca83ce437df97d73fe1eca02d805034c457
marzieh <marziehmonshizade@yahoo.com>
2023-08-28 15:48:29 +0330

    Refactor topbar

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { AiFillSetting } from "react-icons/ai";
-import { GrLanguage } from "react-icons/gr";
 import { IoNotifications } from "react-icons/io5";
+import { MdOutlineLanguage } from "react-icons/md";
+import { Link } from "react-router-dom";
 import "./topbar.css";
 export default function header() {
   return (
-    // <>eee</>
     <div className="d-flex justify-content-end">
-      <a href="#" class="nav-link nav-icon iconClass">
-        <IoNotifications className="m-1 my-3 icon" />
-        <span class="badge badge-dark bg-danger rounded-circle">2</span>
-      </a>
-      <a href="#" class="nav-link nav-icon iconClass">
-        <GrLanguage className="m-1 my-3 icon" />
-        <span class="badge badge-dark bg-danger rounded-circle">8</span>
-      </a>
-      <AiFillSetting className="m-1 my-3 icon" />
+      <Link to="#" className="nav-link nav-icon iconClass">
+        <IoNotifications className="m-1 my-3 iconTopbarStyle icon" />
+        <span className="badge badge-dark bg-danger rounded-circle">2</span>
+      </Link>
+      <Link to="#" className="nav-link nav-icon iconClass">
+        <MdOutlineLanguage className="m-1 my-3 iconTopbarStyle icon" />
+        <span className="badge badge-dark bg-danger rounded-circle">8</span>
+      </Link>
+      <Link to="/">
+        <AiFillSetting className="m-1 my-3 iconTopbarStyle icon" />
+      </Link>
       <img
         src="https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/short/linkedin-profile-picture-maker/dummy_image/thumb/004.webp"
         alt="profile"
